8f875912a6d123d2f8525ed2c40511777716a8e2
Simon Andrews <simon.andrews@babraham.ac.uk>
2021-11-04 10:12:49 +0000

    Fix save location for images

--- training_data/get_training_data.py
+++ training_data/get_training_data.py
@@ -1,14 +1,19 @@
 from google_images_download import google_images_download   
 import json
 from pathlib import Path
+import os
 
 response = google_images_download.googleimagesdownload() 
 
 with open(Path(__file__).parent / "search_terms.json") as st:
     arguments = json.load(st)
 
-print(arguments)
 
 for record in arguments["Records"]:
-    paths = response.download(record) 
-    print(paths)
+    output_folder = Path(__file__).parent / record["folder"]
+    del record["folder"]
+    paths = response.download(record)
+    
+    for path in list(paths[0].values())[0]:
+        file=path.split("\\")[-1]
+        os.rename(path,str(output_folder / file))
